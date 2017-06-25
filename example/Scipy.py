e210e240e282028e251d7adb5d5245f82c05878b
zhouzhou <1197236910@qq.com>
2017-06-25 20:45:29 +0800

    Scipy Basic
    Signed-off-by: zhouzhou <1197236910@qq.com>

---
+++ example/Scipy.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 25 20:04:34 2017
+
+@author: DKjack
+"""
+#SciPy Tutorial
+
+from numpy import poly1d
+p = poly1d([3,4,5])
+print (p) #python3 special
